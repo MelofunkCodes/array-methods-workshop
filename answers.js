f1d50eeef0a1ab79726372e49203679d21cb2086
MelofunkCodes <myeoh27@gmail.com>
2017-01-12 20:24:09 +0000

    Added Exercise 2

--- answers.js
+++ answers.js
@@ -8,4 +8,21 @@ function printPositives (value) {
 
 var a = [ -3, 0.999, -88, 7.2];
 
+console.log("EXERCISE 1: \n");
 a.forEach(printPositives);
+
+//EXERCISE 2
+//use filter method to return a new array with only positive numbers
+function getPositives (value) {
+    if(value > 0) {
+        return value;
+    }
+}
+
+var aFiltered;
+
+aFiltered = a.filter(getPositives);
+
+console.log("\nEXERCISE 2: ");
+console.log("Original array: ", a);
+console.log("Filtered array: ", aFiltered);
